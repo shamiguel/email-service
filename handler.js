2b8ce2eda7be6eb633d08270cce497b0ac5c6cdf
shamito <shamito@google.com>
2024-10-07 17:38:39 -0400

    ?

--- handler.js
+++ handler.js
@@ -3,6 +3,19 @@ const ses = new AWS.SES({ region: 'us-west-2' });
 
 module.exports.sendEmail = async (event) => {
     // inside the `exports.handler.sendEmail` function, before the `return` statement
+    const queryParams = event.queryStringParameters || {};
+    // select from the query parameters
+let { email, message, subject } = queryParams;
+if (!email) {
+    return {
+        statusCode: 400,
+        body: JSON.stringify({
+        message: 'Email is required',
+        }),
+    };
+    }
+    message = message || 'This is a message generated automatically from a Lambda function.';
+subject = subject || 'Hello from Lambda';
 const params = {
     Destination: {
       ToAddresses: ['shamitomita@gmail.com'], // This should be your email address
@@ -21,11 +34,16 @@ const params = {
   };
   await ses.sendEmail(params).promise();
   // in the object that is `return`ed, replace the `body.message` property with `Email sent to ${params.Destination.ToAddresses}`
-    return {
-      statusCode: 200,
-      body: JSON.stringify({
-        message: 'Hello from Appleworld!',
-      }),
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+        {
+        message: `Email sent to ${email} with subject ${subject} and message ${message}`,
+        input: event,
+        },
+        null,
+        2
+    ),
     };
   };
   
